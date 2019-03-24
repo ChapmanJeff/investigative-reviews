bc2d10c8d42a9a1262f3800486fed91544a61858
Jeff Chapman <>
2019-03-23 22:24:02 -0600

    a

--- vertshock/index.php
+++ vertshock/index.php
@@ -1,7 +1,4 @@
-<?php
-  header("Location: http://chapman8.ADAMFOLKER.hop.clickbank.net/")
-?>
-<p>If You're Not Redirected To The Proper Page In 5 Seconds,
+<?php header("Location: http://chapman8.ADAMFOLKER.hop.clickbank.net/")?><p>If You're Not Redirected To The Proper Page In 5 Seconds,
 <a href="http://chapman8.ADAMFOLKER.hop.clickbank.net" target="_top"><strong>CLick 
 Here</strong></a>.
 </p>
