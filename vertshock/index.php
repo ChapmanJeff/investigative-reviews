8bb4d7e6e7715cbf96c1fd48f7785b92213f4c06
Jeff Chapman <>
2019-03-23 22:01:45 -0600

    test

---
+++ vertshock/index.php
@@ -0,0 +1,5 @@
+<?php header("Location: http://chapman8.ADAMFOLKER.hop.clickbank.net/");?>
+<p>If You're Not Redirected To The Proper Page In 5 Seconds,
+<a href="http://chapman8.ADAMFOLKER.hop.clickbank.net" target="_top"><strong>CLick 
+Here</strong></a>.
+</p>
